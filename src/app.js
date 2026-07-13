2d5ca414fabfafdb3db936af6f29c60a6c35b918
Prajyot Fulsundar <prajyot.fulsundar14@gmail.com>
2026-07-13 23:13:18 +0530

    add workflows'

--- src/app.js
+++ src/app.js
@@ -1,16 +1,1 @@
 console.log('hi')
-hhh
-<p align="center">
-  <a href="https://github.com/prajyotfulsundar14/github-actions-capstone/actions/workflows/main-pipeline.yml">
-    <img src="https://github.com/prajyotfulsundar14/github-actions-capstone/actions/workflows/main-pipeline.yml/badge.svg" alt="Main Pipeline Status">
-  </a>
-  <a href="https://github.com/prajyotfulsundar14/github-actions-capstone/actions/workflows/pr-pipeline.yml">
-    <img src="https://github.com/prajyotfulsundar14/github-actions-capstone/actions/workflows/pr-pipeline.yml/badge.svg" alt="PR Pipeline Status">
-  </a>
-  <a href="https://github.com/prajyotfulsundar14/github-actions-capstone/actions/workflows/health-check.yml">
-    <img src="https://github.com/prajyotfulsundar14/github-actions-capstone/actions/workflows/health-check.yml/badge.svg" alt="Health Check Status">
-  </a>
-  <a href="https://hub.docker.com/repository/docker/devopsprajyot/node-app">
-    <img src="https://img.shields.io/docker/pulls/devopsprajyot/node-app.svg" alt="Node App Pulls">
-  </a>
-</p>
